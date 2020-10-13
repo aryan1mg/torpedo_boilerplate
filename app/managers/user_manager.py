24ce2bfebe2a3357c7e1158619df0623c7840989
Ajay Gupta <ajay.gupta@1mg.com>
2020-10-13 16:24:27 +0530

    code refactor

--- app/managers/user_manager.py
+++ app/managers/user_manager.py
@@ -1,6 +1,8 @@
 from ..models import User
 from ..db import ORM
 
+from sanic.exceptions import SanicException
+
 
 class UserManager:
 
@@ -9,7 +11,7 @@ class UserManager:
         username = payload.get('username')
         user = await ORM.get_by_filters(User, {'username': username})
         if not user:
-            raise TypeError('No users found.')
+            raise SanicException('No users found.')
         user = await user[0].to_dict()
         return user
 
